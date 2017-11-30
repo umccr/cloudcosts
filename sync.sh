8a870d8970f9eb62372d74ab68006885ec085447
Roman Valls Guimera <brainstorm@nopcode.org>
2017-12-01 10:03:20 +1100

    Consolidate individual logs in just one logfile, as supported by datadog

--- sync.sh
+++ sync.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-rsync -a --progress raijin:iperf-runs/*.log .
-rsync -a --progress spartan:iperf-runs/*.log .
+rsync -a --progress raijin:iperf-runs/*.log logs/
+rsync -a --progress spartan:iperf-runs/*.log logs/
+
+egrep receiver logs/*.log | grep nci > hpc_networks.log
